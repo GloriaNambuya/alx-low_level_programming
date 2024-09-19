331eb2eda14ad7a7e15f7efed5dba2558f38d24a
GloriaNambuya <glorianash983@gmail.com>
2024-09-19 04:33:27 -0700

    print hexadecimal

---
+++ 0x01-variables_if_else_while/8-8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print hexadecimal numbers
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char d = '0';
+	char i = 'a';
+
+	while (d <= '9')
+	{
+		putchar(d++);
+	}
+	while (i <= 'f')
+	{
+		putchar(i++);
+	}
+	putchar('\n');
+	return (0);
+}
